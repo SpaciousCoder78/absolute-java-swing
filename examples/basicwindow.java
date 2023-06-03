53f2ef0b4385ddba069f912e661e7df451a0163d
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2023-06-03 18:48:06 +0530

    Create basicwindow.java

---
+++ examples/basicwindow.java
@@ -0,0 +1,10 @@
+//BASIC JFrame
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame= new JFrame("newwindow");
+        frame.setVisible(true);
+        frame.setSize(400,400);
+
+    }
+}
